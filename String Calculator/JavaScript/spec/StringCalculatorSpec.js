efa495386f77a2251e1ac59766daa555f21c233a
Diego Guemes <diegogp1988@gmail.com>
2013-03-05 11:01:08 +0100

    Refactoring test code

--- String Calculator/JavaScript/spec/StringCalculatorSpec.js
+++ String Calculator/JavaScript/spec/StringCalculatorSpec.js
@@ -1,12 +1,15 @@
 describe('A String Calculator', function(){
+  var calculator;
+  beforeEach(function(){
+    calculator = new StringCalculator();
+  });
+
   it("should return zero when adding an empty string", function(){
-    var calculator = new StringCalculator();
     var result = calculator.add("");
     expect(result).toBe(0);
   });
 
   it("should return the number when adding one number", function(){
-    var calculator = new StringCalculator();
     var result = calculator.add("10");
     expect(result).toBe(10);
   });
